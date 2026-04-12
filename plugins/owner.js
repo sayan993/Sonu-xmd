7ca1a848cdd0dc90fe4c63acd5ac3a1f0e6a9d91
sayan993 <yoursayan801@gmail.com>
2026-04-12 11:55:47 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -29,8 +29,8 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/yj7zp0.png' }, // Image URL from your request
-            caption: `╭━━〔 *𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃* 〕━━┈⊷
+            image: { url: 'https://files.catbox.moe/g8tadq.bin' }, // Image URL from your request
+            caption: `╭━━〔 *the-sonu-xmd* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
 ┃◈┃• *Name* - ${ownerName}
@@ -45,7 +45,7 @@ async (conn, mek, m, { from }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: '*𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃*',
+                    newsletterName: '*the-sonu-xmd*',
                     serverMessageId: 143
                 }            
             }
